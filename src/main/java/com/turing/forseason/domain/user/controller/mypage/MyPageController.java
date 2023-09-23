b10a599fda6a785efc1e1eca32f03a90d14eae8c
Yeonjae Choi <fox2yj@naver.com>
2023-09-23 16:27:54 +0900

    feat: MyPageController 파라미터 수정 #38

--- src/main/java/com/turing/forseason/domain/user/controller/mypage/MyPageController.java
+++ src/main/java/com/turing/forseason/domain/user/controller/mypage/MyPageController.java
@@ -7,7 +7,9 @@ import com.turing.forseason.domain.user.service.UserService;
 import com.turing.forseason.global.dto.ApplicationResponse;
 import com.turing.forseason.global.errorException.CustomException;
 import com.turing.forseason.global.errorException.ErrorCode;
+import com.turing.forseason.global.jwt.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,29 +23,29 @@ public class MyPageController {
     private final UserRepository userRepository;
 
     @GetMapping("/mypage/{userId}")
-    public ApplicationResponse<UserEntity> getUser(@PathVariable long userId) {
+    public ApplicationResponse<UserEntity> getUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         //마이페이지 보기
 
-        UserEntity user = userService.getUserById(userId);
+        UserEntity user = userService.getUserById(principalDetails.getUser().getUserId());
 
         return ApplicationResponse.ok(ErrorCode.SUCCESS_OK, user);
     }
 
     @GetMapping("/mypage/editprofile/{userId}")
-    public ApplicationResponse<UserEntity> getBeforeEditUser(@PathVariable long userId) {
+    public ApplicationResponse<UserEntity> getBeforeEditUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         //회원정보 수정 보기
 
-        UserEntity user = userService.getUserById(userId);
+        UserEntity user = userService.getUserById(principalDetails.getUser().getUserId());
 
         return ApplicationResponse.ok(ErrorCode.SUCCESS_OK, user);
     }
 
     @PostMapping("/mypage/editprofile/{id}")
-    public ApplicationResponse<UserEntity> editUser(@PathVariable long userId, @RequestBody UpdateUserInfoRequest request) {
+    public ApplicationResponse<UserEntity> editUser(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody UpdateUserInfoRequest request) {
         //회원정보 수정
 
         // UserEntity의 update 메서드 호출 등 업데이트 작업 수행
-        UserEntity updatedUserInfo = userService.update(userId, request);
+        UserEntity updatedUserInfo = userService.update(principalDetails.getUser().getUserId(), request);
 
         // 업데이트된 user를 저장
         userRepository.save(updatedUserInfo);
