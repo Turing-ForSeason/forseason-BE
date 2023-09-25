d305a5c171b7ac9a10dd194a308c24a3ac63cbb9
Yeonjae Choi <fox2yj@naver.com>
2023-09-25 16:18:41 +0900

    feat: MyPageController editUser 메서드 로직 수정 #38

--- src/main/java/com/turing/forseason/domain/user/controller/mypage/MyPageController.java
+++ src/main/java/com/turing/forseason/domain/user/controller/mypage/MyPageController.java
@@ -2,7 +2,6 @@ package com.turing.forseason.domain.user.controller.mypage;
 
 import com.turing.forseason.domain.user.dto.UpdateUserInfoRequest;
 import com.turing.forseason.domain.user.entity.UserEntity;
-import com.turing.forseason.domain.user.repository.UserRepository;
 import com.turing.forseason.domain.user.service.UserService;
 import com.turing.forseason.global.dto.ApplicationResponse;
 import com.turing.forseason.global.errorException.ErrorCode;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 public class MyPageController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
 
     @GetMapping("/mypage")
     public ApplicationResponse<UserEntity> getUser(@AuthenticationPrincipal PrincipalDetails principalDetails) {
@@ -42,9 +40,6 @@ public class MyPageController {
 
         // UserEntity의 update 메서드 호출 등 업데이트 작업 수행
         UserEntity updatedUserInfo = userService.update(principalDetails.getUser().getUserId(), request);
-
-        // 업데이트된 user를 저장
-        userRepository.save(updatedUserInfo);
 
         return ApplicationResponse.ok(ErrorCode.SUCCESS_OK, updatedUserInfo);
     }
